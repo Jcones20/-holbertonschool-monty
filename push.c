5925fb1fb2c8d138a196e857ae42b13e0fe7dd7a
root <Joshcones1253@yahoo.com>
2023-08-23 23:25:40 -0700

    cowboys

--- push.c
+++ push.c
@@ -1,22 +1,53 @@
-#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void push(stack_t **stack, int value) {
-    
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (!new_node) {
-        fprintf(stderr, "Error: malloc failed\n");
+#define STACK_SIZE 100
+
+int stack[STACK_SIZE];
+int top = -1;
+
+void push(int value) {
+    if (top >= STACK_SIZE - 1) {
+        fprintf(stderr, "Stack overflow\n");
         exit(EXIT_FAILURE);
     }
 
-    new_node->n = value;
-    new_node->prev = NULL;
+    stack[++top] = value;
+}
 
-    if (!*stack) {
-        new_node->next = NULL;
-    } else {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
+void pall() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d\n", stack[i]);
+    }
+}
+
+int main() {
+    FILE *file = fopen("program.txt", "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        return 1;
     }
 
-    *stack = new_node;
+    char opcode[10];
+    int value;
+
+    while (fscanf(file, "%s", opcode) == 1) {
+        if (strcmp(opcode, "push") == 0) {
+            if (fscanf(file, "%d", &value) != 1) {
+                fprintf(stderr, "Error parsing argument for push\n");
+                return 1;
+            }
+            push(value);
+        } else if (strcmp(opcode, "pall") == 0) {
+            pall();
+        } else {
+            fprintf(stderr, "Unknown opcode: %s\n", opcode);
+            return 1;
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
+
